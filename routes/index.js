06ec02d0dc88ecd9567a6e0ede90e31946c59199
Aaron Aichlmayr <waterfoul@gmail.com>
2017-01-27 14:55:16 -0600

    Added a basic view

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 'use strict';
 
 const express = require('express');
+const Bluebird = require('bluebird');
+
+const models = require('../models');
 
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('index');
+	Bluebird.all([
+		models.hotel.findAll(),
+		models.restaurant.findAll(),
+		models.activity.findAll()
+	]).spread((hotels, restaurants, activities) => {
+		res.render('index', {
+			hotels: hotels,
+			restaurants: restaurants,
+			activities: activities
+		});
+	});
 });
 
 module.exports = router;
--- static/index.js
+++
@@ -1,1 +0,0 @@
-alert('Static Success!');
