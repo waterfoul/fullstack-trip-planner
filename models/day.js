45622e1c6ea444cf32d9ca5a9597eeebcf3fbdd7
Aaron Aichlmayr <waterfoul@gmail.com>
2017-02-02 09:07:42 -0600

    Fixing the day model

--- models/day.js
+++ models/day.js
@@ -13,4 +13,4 @@ Day.belongsTo(Hotel);
 Day.belongsToMany(Restaurant, {through: 'day_restaurant'});
 Day.belongsToMany(Activity, {through: 'day_activity'});
 
-module.exports = Hotel;
+module.exports = Day;
